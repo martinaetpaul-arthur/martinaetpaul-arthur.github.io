9bf097506e317dba986cacbef4a666ca34d3c7cd
Stefano Bielli <stefano.bielli@edreamsodigeo.com>
2023-02-09 00:14:50 +0100

    fix header position

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import { IconButton, Stack, Switch, Typography } from '@mui/material';
+import { Container, Grid, IconButton, Switch, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Toolbar from '@mui/material/Toolbar';
 import * as React from 'react';
 
 import { Locale } from './Locale.js';
@@ -11,40 +10,48 @@ function Header({ pages, onMenuClick, onHomeClick }) {
 
     const { setLocale } = React.useContext(Locale);
 
-    const updateLocale = event => {
-        if (event.target.checked) {
-            setLocale('en')
-        } else {
-            setLocale('it')
-        }
+    const updateLocale = (event) => {
+        event.target.checked ? setLocale('en') : setLocale('it');
     }
 
     return (
         <Box id="header" component="header" >
-            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <IconButton size="large" sx={{ display: { xs: 'flex', md: 'none' } }}>
-                    <MenuIcon />
-                </IconButton>
+            <Container sx={{ height: 64 }} >
+                <Grid container columns={{ xs: 4, md: 12 }} justifyContent="space-between">
+                    <Grid item xs={1} sx={{ display: { xs: 'flex', md: 'none' } }} >
+                        <IconButton size="large" >
+                            <MenuIcon />
+                        </IconButton>
+                    </Grid>
+                    <Grid item xs={2} sx={{ display: { xs: 'flex', md: 'none' } }} justifyContent="center">
+                        <Box sx={{ height: 64 }} component="img" src="/static/logo.png" />
+                    </Grid>
 
-                <Box sx={{ display: { xs: 'none', md: 'flex' }, height: 64, cursor: 'pointer' }} onClick={onHomeClick} component="img" src="/static/logo.png" />
-                <Box sx={{ display: { xs: 'none', md: 'flex' } }} alignItems="center" display="flex">
-                    {pages.map((page) => (
-                        <Button
-                            key={page}
-                            onClick={() => onMenuClick(page)}
-                        >
-                            {page}
-                        </Button>
-                    )
-                    )}
-                </Box>
+                    <Grid item md={1} sx={{ display: { xs: 'none', md: 'flex' }, }}>
+                        <Box sx={{ height: 64, cursor: 'pointer' }} onClick={onHomeClick} component="img" src="/static/logo.png" />
+                    </Grid>
+                    <Grid item md={10} sx={{ display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
 
-                <Stack height="100%" direction="row" justifyItems="center" alignItems="center">
-                    <Typography color="primary">IT</Typography>
-                    <Switch onChange={updateLocale} size="small"></Switch>
-                    <Typography color="primary">EN</Typography>
-                </Stack>
-            </Toolbar>
+                        <Box alignItems="center" display="flex">
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={() => onMenuClick(page)}
+                                >
+                                    {page}
+                                </Button>
+                            )
+                            )}
+                        </Box>
+                    </Grid>
+
+                    <Grid item xs={1} alignItems="center" justifyContent="right" display="flex">
+                        <Typography color="primary">IT</Typography>
+                        <Switch onChange={updateLocale} size="small" />
+                        <Typography color="primary">EN</Typography>
+                    </Grid>
+                </Grid>
+            </Container>
         </Box>
     );
 }
