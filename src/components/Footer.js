4e46cfe3c8bc9c431d96bf443f7179f8fd0b2038
Stefano Bielli <stefano.bielli@edreamsodigeo.com>
2023-02-09 01:41:02 +0100

    optimized footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,27 @@
 import CodeIcon from '@mui/icons-material/Code';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { Box, Link, Typography } from '@mui/material';
+import { Box, Grid, Link, Typography } from '@mui/material';
 import React from 'react';
 
 function Footer() {
-    
+
     return (
-        <Box component='footer' id="footer" padding={5} justifyContent="center" display="flex">
-            <Typography id="placeholder" variant='caption' display="flex">
-                <CodeIcon fontSize='small' sx={{ marginRight: 1 }} />Developed by us.
-            </Typography>
-            <Typography id="placeholder" variant='caption' display="flex">
-                <Link href="https://github.com/stebielli/wedding" color="inherit" underline="hover" display="flex" >
-                    <GitHubIcon fontSize='small' sx={{ marginLeft: 1, marginRight: 1 }} />
-                    Code on GitHub.
-                </Link>
-            </Typography>
+        <Box component='footer' id="footer" padding={2}>
+            <Grid container justifyContent="center" >
+                <Grid item >
+                    <Typography id="placeholder" padding={1} variant='caption' display="flex" >
+                        <CodeIcon fontSize='small' sx={{ marginRight: 1 }} />Developed by us.
+                    </Typography>
+                </Grid>
+                <Grid item >
+                    <Typography id="placeholder" padding={1} variant='caption' display="flex">
+                        <Link href="https://github.com/stebielli/wedding" color="inherit" underline="hover" display="flex" >
+                            <GitHubIcon fontSize='small' sx={{ marginRight: 1 }} />
+                            Code on GitHub.
+                        </Link>
+                    </Typography>
+                </Grid>
+            </Grid>
         </Box>
 
     );
