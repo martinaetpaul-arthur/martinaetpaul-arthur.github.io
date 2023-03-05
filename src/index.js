25cfbd2f5a997137c242272ab4a818242f6085c5
Stefano Bielli <stefano.bielli@edreamsodigeo.com>
2023-03-05 23:55:49 +0100

    use hash router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App page={'home'}/>,
