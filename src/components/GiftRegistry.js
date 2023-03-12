f97417999a50a7aeb2a9ef347defa3be06e79e90
Stefano Bielli <stefano.bielli@edreamsodigeo.com>
2023-03-12 14:19:10 +0100

    fix paddings

--- src/components/GiftRegistry.js
+++ src/components/GiftRegistry.js
@@ -8,18 +8,18 @@ function GiftRegistry() {
     return (
         <Container id="gift-registry">
             <Box margin='auto' maxWidth={600}>
-                <Box padding={3} display='flex' flexDirection='column'>
+                <Box paddingTop={3}  paddingBottom={3}display='flex' flexDirection='column'>
                     <Box display='flex' justifyContent='center' marginBottom={4}>
                         <Box component='img' src='static/gift_registry.png' maxWidth={200} />
                     </Box>
-                    <Typography textAlign='center'>{t('gift.saintAugustine1')}</Typography>
-                    <Typography textAlign='center' fontStyle='italic' fontSize={14}>{t('gift.saintAugustine2')}</Typography>
+                    <Typography textAlign={{ xs: 'justify', sm: 'center' }}>{t('gift.saintAugustine1')}</Typography>
+                    <Typography textAlign={{ xs: 'right', sm: 'center' }} fontStyle='italic' fontSize={14}>{t('gift.saintAugustine2')}</Typography>
                 </Box>
                 <Divider sx={{ bgcolor: "primary.main", maxWidth: 200, margin: 'auto' }} />
                 <Box justifyContent='center'>
-                    <Box margin={4}>
-                        <Typography textAlign='center' fontWeight='bold' paddingBottom={2}>{t('gift.content1')}</Typography>
-                        <Typography textAlign='center'>{t('gift.content2')}</Typography>
+                    <Box paddingTop={4}  paddingBottom={4}>
+                        <Typography textAlign={{ xs: 'justify', sm: 'center' }} fontWeight='bold' paddingBottom={2}>{t('gift.content1')}</Typography>
+                        <Typography textAlign={{ xs: 'justify', sm: 'center' }}>{t('gift.content2')}</Typography>
                     </Box>
                     <Box border={1} borderColor='primary.main' maxWidth={300} margin='auto' padding={0.5} display='flex' flexDirection='column'>
                         <Typography textAlign='left' fontFamily='Roboto' variant='caption'><Box display='inline' fontWeight='bold'>{t('gift.accountHolder')}:</Box> STEFANO BIELLI</Typography>
