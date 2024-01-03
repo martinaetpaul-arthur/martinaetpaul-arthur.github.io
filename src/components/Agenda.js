37ea85fe9c845c8586b10930c61034768699a0d9
Stefano Bielli <stefano.bielli@edreamsodigeo.com>
2024-01-03 14:58:30 +0100

    changed agenda and home

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,4 +1,4 @@
-import { Box, Link, Stack, Typography } from '@mui/material';
+import { Box, Divider, Link, Stack, Typography } from '@mui/material';
 
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -9,27 +9,37 @@ function Agenda() {
 
     return (
         <Box id="agenda" minWidth={{ md: '80%' }} flex>
-            <Stack direction={{ sm: 'column', md: 'row' }} flex justifyContent='center'>
-                <Box height={{ md: 350 }} width={{ md: 200 }}>
-                    <Box display='flex' height={140} padding={2}>
+            <Stack direction={{ sm: 'column', md: 'row' }} flex justifyContent='space-around' paddingBottom={4}>
+                <Box width={{ md: 200 }}>
+                    <Box display='flex' height={140} >
                         <Box margin='auto' component='img' maxWidth={70} src='/static/calendar.png' />
                     </Box>
                     <Typography textAlign='center' fontWeight='bold' paddingBottom={2}>{t('agenda.weddingDay')}</Typography>
-                    <Typography textAlign='center' paddingBottom={2}>{t('agenda.weddingDate')}</Typography>
+                    <Typography textAlign='center' >{t('agenda.weddingDate')}</Typography>
                 </Box>
-                <Box height={{ md: 350 }} width={{ md: 200 }} />
-                <Box height={{ md: 350 }} width={{ md: 200 }} >
-                    <Box display='flex' height={140} padding={2}>
+                <Box width={{ md: 200 }} >
+                    <Box display='flex' height={140} >
+                        <Box margin='auto' component='img' maxWidth={85} src='/static/cerimony.png' />
+                    </Box>
+                    <Typography textAlign='center' fontWeight='bold' paddingBottom={2}>{t('agenda.cerimony')}</Typography>
+                    <Typography textAlign='center' >{t('agenda.cerimonyDescription')}</Typography>
+                </Box>
+                <Box width={{ md: 200 }} >
+                    <Box display='flex' height={140} >
                         <Box margin='auto' component='img' maxWidth={60} src='/static/fireworks.png' />
                     </Box>
                     <Typography textAlign='center' fontWeight='bold' paddingBottom={2}>{t('agenda.reception')}</Typography>
-                    <Typography textAlign='center' paddingBottom={2}>{t('agenda.receptionTime')}</Typography>
-                    <Typography textAlign='center'><Link href="https://www.villaquintili.com/">{t('agenda.receptionDirection1')}</Link></Typography>
-                    <Typography textAlign='center'>{t('agenda.receptionDirection2')}</Typography>
-                    <Typography textAlign='center'>{t('agenda.receptionDirection3')}</Typography>
+                    <Typography textAlign='center' >{t('agenda.receptionDescription')}</Typography>
                 </Box>
             </Stack>
-            <Box paddingTop={{ xs: 4, sm: 3 }} width={{ xs: '80vw', md: '100%' }} >
+            <Divider sx={{ bgcolor: "primary.main", maxWidth: 600, margin: 'auto' }} />
+            <Box width={{ xs: '80vw', md: '100%' }} >
+                <Stack display='flex' paddingTop={4} paddingBottom={4}>
+                    <Box margin='auto' component='img' paddingBottom={2} maxWidth={70} src='/static/location.png' />
+                    <Typography textAlign='center' fontWeight='bold' paddingBottom={2}><Link href="https://www.villaquintili.com/">{t('agenda.direction1')}</Link></Typography>
+                    <Typography textAlign='center'>{t('agenda.direction2')}</Typography>
+                    <Typography textAlign='center'>{t('agenda.direction3')}</Typography>
+                </Stack>
                 <GoogleMaps />
             </Box>
         </Box>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,41 @@
-import { Box, Button, Grid, Link, Typography } from '@mui/material';
+import { Box, Button, Container, Divider, Grid, Link, Typography } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Locale } from './Locale';
 
 function Home({ onRsvpClick }) {
+    const locale = React.useContext(Locale);
     const { t } = useTranslation();
 
     return (
-        <Box id="home">
-            <Grid container direction="column" justifyContent="space-between" alignItems="center" rowSpacing={2}>
-                <Grid item>
-                    <Box sx={{ objectFit: 'contain', maxWidth: { xs: 350, sm: 550 } }} component="img" src="/static/love.png" />
+        <Container id="home">
+            <Box margin="auto" maxWidth={600}>
+                <Grid container direction="column" justifyContent="space-between" alignItems="center" rowSpacing={2}>
+                    <Grid item>
+                        <Box hidden={locale === 'it'} sx={{ objectFit: 'contain', maxWidth: { xs: 300, sm: 550 } }} component="img" src="/static/love_fr.png" />
+                        <Box hidden={locale !== 'it'} sx={{ objectFit: 'contain', maxWidth: { xs: 300, sm: 550 } }} component="img" src="/static/love_it.png" />
+                    </Grid>
+                    <Grid item>
+                        <Typography textAlign={{ xs: 'justify', sm: 'center' }} variant='h6' paddingBottom={3}>{t('home.description')}</Typography>
+                        <Divider sx={{ bgcolor: "primary.main", maxWidth: 250, margin: 'auto' }} />
+                    </Grid>
+                    <Grid item>
+                        <Typography textAlign='center' variant='h6' paddingTop={1}>{t('home.weddingDirection')}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button onClick={onRsvpClick} variant="contained" disableElevation>
+                            <Typography>
+                                RSVP
+                            </Typography>
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Typography textAlign='center' >{t('home.questions')}</Typography>
+                        <Typography paddingBottom={1} textAlign='center' ><Link href='mailto:mariage.martina.pa@gmail.com'>mariage.martina.pa@gmail.com</Link></Typography>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Typography textAlign='center' variant='h6'>{t('home.weddingDirection')}</Typography>
-                </Grid>
-                <Grid item>
-                    <Button onClick={onRsvpClick} variant="contained" disableElevation>
-                        <Typography>
-                            RSVP
-                        </Typography>
-                    </Button>
-                </Grid>
-                <Grid item>
-                    <Typography textAlign='center' >{t('home.questions')}</Typography>
-                    <Typography paddingBottom={1} textAlign='center' ><Link href='mailto:mariage.martina.pa@gmail.com'>mariage.martina.pa@gmail.com</Link></Typography>
-                </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </Container>
     );
 }
 
